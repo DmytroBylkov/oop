a7634456f3c561214bae550bf0e72485404ab0b8
Былков Дмитрий <bylkovd@gmail.com>
2022-11-28 14:38:37 +0200

    made changes according to the recommendations. only in the Product class left method

--- main.py
+++ main.py
@@ -2,43 +2,69 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+Challenge:
+#. Implement ``Product`` class
+#. Each ``Product`` instance should implement properties:
+    * ``name`` - a product's name, like apple, cheese etc.
+    * ``price`` - a price for a single unit
+#. ``Product`` instance should have a behavior of calculating total
+   price for a specified quantity of goods
+#. Implement ``ShoppingCart`` class
+#. ``ShoppingCart`` instance should combine products instances and
+    corresponding purchased quantities.
+#. ``ShoppingCart`` instance should implement a method to calculate
+   the total price of entire cart.
+"""
+
 class Product:
-    def __init__(self, name: str, price: int, amount: int):
+    """class Product imlementation
+    * ``name`` - a product's name, like apple, cheese etc.
+    * ``price`` - a price for a single unit
+    """
+    def __init__(self, name: str, price: int):
         self.name = name
         self.price = price
-        self.amount = amount
+
+
+    def calculating_total_price_for_product(self, quantity_of_goods):
+        return quantity_of_goods * self.price
+
+
+class ShoppingCart:
+    """class ShoppingCart implementation
+    ``ShoppingCart`` instance should combine products instances and
+    corresponding purchased quantities.
+    ``ShoppingCart`` instance should implement a method to calculate
+   the total price of entire cart.
+    """
+    products_in_cart = []
+
+
+    def add_product(self, obj, amount):
+        self.products_in_cart.append((obj.name,obj.price, amount))
+
 
     def total_price(self):
-        return self.amount * self.price
-
-
-class SmartBasket:
-    product_in_basket = {}
-
-    def add_product(self,obj):
-        self.product_in_basket.update([(obj.name, obj.total_price())])
-
-    @property
-    def totalCostOfProducts(self):
         sum = 0
-        for product in self.product_in_basket:
-            sum = sum + self.product_in_basket[product]
+        for product in self.products_in_cart:
+            sum = sum + product[1] * product[2]
         return sum
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    apple = Product('apple', 10, 5)
+    apple = Product('apple', 10)
+    print(apple.calculating_total_price_for_product(5))
+    pear = Product('pear', 15)
+    orange = Product('orange', 15)
+    cart = ShoppingCart()
+    cart.add_product(apple, 2)
+    print(cart.products_in_cart)
+    cart.add_product(pear, 1)
+    cart.add_product(orange, 4)
+    print(cart.total_price())
 
-    pear = Product('pear', 15, 3)
-
-    orange = Product('orange', 15, 2)
-
-    basket = SmartBasket()
-    basket.add_product(apple)
-    basket.add_product(pear)
-    basket.add_product(orange)
-    print(basket.totalCostOfProducts)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
